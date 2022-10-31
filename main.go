45c3628a93055577c7df5608f249a6ebd7a94edd
gic-rishabh <101700635+gic-rishabh@users.noreply.github.com>
2022-10-31 15:28:14 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.ListenAndServe(":4000", nil)
+}
+
+func foo(w http.ResponseWriter, req *http.Request) {
+	var s string
+	if req.Method == http.MethodPost {
+		f, _, err := req.FormFile("usrfile")
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Error uploading fle", http.StatusInternalServerError)
+			return
+		}
+		defer f.Close()
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			log.Println(err)
+			http.Error(w, "Error reading file", http.StatusInternalServerError)
+			return
+		}
+		s = string(bs)
+	}
+
+	w.Header().Set("CONTENT-TYPE", "text/html;charset=UTF-8")
+	fmt.Fprintf(w, `<form action="/" method="POST" enctype="multipart/form-data">
+	   upload a file<br>
+	   <input  type="file" name="usrfile">
+	   <input type="submit">
+       </form>
+	   <br>
+	   <br>
+	   <h1>%v</h1>`, s)
+
+}
